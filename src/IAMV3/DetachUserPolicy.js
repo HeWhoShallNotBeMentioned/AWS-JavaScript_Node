a020402d125d5a36547c151b97b1903146687b4d
Chris Underwood <cunderwoodmn@gmail.com>
2023-06-21 20:57:34 -0700

    create DetachUserPolicy.js SDK V3.

---
+++ src/IAMV3/DetachUserPolicy.js
@@ -0,0 +1,17 @@
+import { IAMClient, DetachUserPolicyCommand } from '@aws-sdk/client-iam';
+
+const client = new IAMClient();
+
+(async () => {
+  const params = {
+    UserName: 'newuser2',
+    PolicyArn: 'arn:aws:iam::342445434384:policy/V3FullAccessPolicy',
+  };
+
+  try {
+    const response = await client.send(new DetachUserPolicyCommand(params));
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+})();
