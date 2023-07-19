a7e10e6bf31559a2b96096d70b82b499becaf48f
Chris Underwood <cunderwoodmn@gmail.com>
2023-07-18 17:41:05 -0700

    create folder S3V2 and CreateBucket.js for AWS SDK V2

---
+++ src/S3V2/CreateBucket.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({ region: 'us-west-2' });
+// can also put accessKeyId and secretAccessKey in config fiel with region
+
+const params = {
+  Bucket: 'testingbucket4clu',
+  ACL: 'private',
+
+  CreateBucketConfiguration: { LocationConstraint: 'us-west-2' },
+};
+
+s3.createBucket(params, function (err, data) {
+  if (err) {
+    console.log('\n\nERROR-----------\n\n', err, err.stack);
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n  Bucket Created\n\n', data);
+  }
+});
