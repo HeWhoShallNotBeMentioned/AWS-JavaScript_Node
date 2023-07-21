c7ac0bc47e6f6754c2a4438943ce9732e027c1c1
Chris Underwood <cunderwoodmn@gmail.com>
2023-07-20 18:20:02 -0700

    add UploadImage.js and 3 images to test with.

---
+++ src/S3V2/UploadImage.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({ region: 'us-west-2' });
+
+var params = {
+  Body: fs.createReadStream('./funny.jpg'),
+  Bucket: 'testingbucket3clu',
+  Key: 'funny.jpg',
+  ACL: 'private',
+  ContentType: 'application/octet-stream',
+};
+
+s3.putObject(params, function (err, data) {
+  if (err) {
+    console.log('\n\nERROR-----------\n\n', err, err.stack);
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n  Image Uploaded\n\n', data);
+  }
+});
