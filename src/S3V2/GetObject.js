0d89483bb8cb5465bf1af54be58d5ba3c42e0374
Chris Underwood <cunderwoodmn@gmail.com>
2023-07-21 17:48:35 -0700

    create GetObject.js via AWS SDK V2

---
+++ src/S3V2/GetObject.js
@@ -0,0 +1,12 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({ region: 'us-west-2' });
+const params = { Bucket: 'testingbucket2clu', Key: 'moviedata.json' };
+
+s3.getObject(params, function (err, data) {
+  if (err) {
+    console.log('\n\nERROR-----------\n\n', err, err.stack);
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n  File retrieved!\n\n', data);
+  }
+});
