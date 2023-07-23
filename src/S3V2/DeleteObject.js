c920d4d24d8961986da2866570d545ea2549907f
Chris Underwood <cunderwoodmn@gmail.com>
2023-07-22 17:07:16 -0700

    create DeleteObject.js via AWS SDK V2 S3

---
+++ src/S3V2/DeleteObject.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({ region: 'us-west-2' });
+
+const params = { Bucket: 'testingbucket1clu', Key: 'funny.jpg' };
+
+s3.deleteObject(params, function (err, data) {
+  if (err) {
+    console.log('\n\nERROR-----------\n\n', err, err.stack);
+  } else {
+    console.log(
+      `\n\nSUCCESS!!!\n\n  Object ${params.Bucket} ${params.Key} deleted!\n\n`,
+      data
+    );
+  }
+});
