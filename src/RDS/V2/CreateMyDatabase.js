910f3e1bd2a39561149b7db9113aa916cd3da6f6
Chris Underwood <cunderwoodmn@gmail.com>
2023-08-04 22:20:40 -0700

    create CreateMyDatabase.js via AWS SDK V2 for RDS

---
+++ src/RDS/V2/CreateMyDatabase.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const con = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.RDS_Database_Password,
+});
+
+con.connect(function (err, data) {
+  if (err) {
+    console.log(
+      '\n\nERROR-----------DB Instance Not Connected\n\n',
+      err,
+      '\n\n   ',
+      err.stack
+    );
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n    DB Instance Connected\n\n', data);
+    con.query('CREATE DATABASE TESTDB', function (err, result) {
+      if (err) {
+        console.log(
+          'err ----------------- \n\n          ',
+          err.message,
+          '\n\n',
+          err
+        );
+      } else {
+        console.log('Database created.\n\n    ', data);
+      }
+    });
+  }
+});
