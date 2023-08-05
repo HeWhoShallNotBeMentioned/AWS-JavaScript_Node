532acd17a8de3c30cef8f836f1d3dfd343ab5b52
Chris Underwood <cunderwoodmn@gmail.com>
2023-08-04 21:42:20 -0700

    created file to connect to the AWS database.

---
+++ src/RDS/V2/CreateDatabaseConnection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+console.log('password', process.env.RDS_Database_Password);
+
+const con = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.RDS_Database_Password,
+});
+
+con.connect(function (err, data) {
+  if (err) {
+    console.log(
+      '\n\nERROR-----------DB Instance Not Connected\n\n',
+      err,
+      '\n\n   ',
+      err.stack
+    );
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n    DB Instance Connected\n\n', data);
+  }
+});
