487c186ae4e5404314c2241b27eae2edb67dab9e
Chris Underwood <cunderwoodmn@gmail.com>
2023-08-05 17:58:38 -0700

    create SelectData.js via AWS SDK V2 for RDS

---
+++ src/RDS/V2/SelectData.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const con = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.RDS_Database_Password,
+  database: process.env.database,
+});
+
+con.connect(function (err, data) {
+  if (err) {
+    console.log(
+      '\n\nERROR-----------DB Instance Not Connected\n\n',
+      err,
+      '\n\n   ',
+      err.stack
+    );
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n    DB Instance Connected\n\n', data);
+    const sql = 'SELECT * FROM customers WHERE name="Monty"';
+
+    con.query(sql, function (err, result) {
+      if (err) {
+        console.log(
+          'err ----------------- \n\n          ',
+          err.message,
+          '\n\n',
+          err
+        );
+      } else {
+        console.log(
+          `\n\nSUCCESS!!!\n\n    Data retreived from ${process.env.database}\n\n`,
+          result
+        );
+      }
+    });
+  }
+});
