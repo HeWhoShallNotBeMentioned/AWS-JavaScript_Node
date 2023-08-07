c6b3562fa5ce8d1942d6138adb14c45b3d935799
Chris Underwood <cunderwoodmn@gmail.com>
2023-08-06 23:24:55 -0700

    create DeleteSnapshot.js AWS RDS SDK V3

---
+++ src/RDS/MySQL/V3/DeleteSnapshot.js
@@ -0,0 +1,17 @@
+import { RDSClient, DeleteDBSnapshotCommand } from '@aws-sdk/client-rds';
+const client = new RDSClient({ region: 'us-west-2' });
+(async () => {
+  const input = {
+    DBSnapshotIdentifier: 'test-mysql-instance-snapshot',
+  };
+
+  try {
+    const response = await client.send(new DeleteDBSnapshotCommand(input));
+    console.log(
+      `  Snapshot ${DBSnapshotIdentifier} Deleted----------------------- \n\n  `,
+      response
+    );
+  } catch (err) {
+    console.log('\n\nError.....\n\n', err.message, err, err.stack);
+  }
+})();
