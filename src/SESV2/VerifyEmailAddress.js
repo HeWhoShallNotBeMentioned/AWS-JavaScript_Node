88ebdcf2547f041b41a22f1a53f24ef160bf6a17
Chris Underwood <cunderwoodmn@gmail.com>
2023-10-01 16:28:03 -0700

    add VerifyEmailAddress.js using AWS SDK V2 for javascript in new SESV2 folder

---
+++ src/SESV2/VerifyEmailAddress.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+
+const ses = new AWS.SES({ region: 'us-west-2' });
+
+var params = {
+  EmailAddress: 'cunderwoodmn@gmail.com',
+};
+ses.verifyEmailIdentity(params, function (err, data) {
+  if (err) {
+    console.log(
+      '\n\n     ERROR!!! email not verfified -----------\n\n ',
+      err,
+      err.stack
+    );
+  } else {
+    console.log('\n\nSUCCESS!!!\n\n  Email Verified\n\n', data);
+  }
+});
+
+  // "type": "module",
